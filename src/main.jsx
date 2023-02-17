58c4c48e971b4f248f4eb9a17fff3095538f851c
Dan <vareladanmarj@gmail.com>
2023-02-17 19:21:22 +0800

    feat: add home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App'
 import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from '@pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 )
--- src/tests/sample.test.js
+++
@@ -1,5 +0,0 @@
-import { expect, it } from 'vitest'
-
-it('should work as expected', () => {
-  expect(Math.sqrt(4)).toBe(2)
-})
