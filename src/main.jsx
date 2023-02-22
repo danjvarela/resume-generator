f9f1eac3294f76e5cdcc410d7710c064b67bd470
percival49 <percivalsalvador@gmail.com>
2023-02-22 19:28:58 +0800

    feat:add-signup-page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from '@pages/Home'
 import Login from '@pages/Login'
+import Signup from '@pages/Signup'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
   },
 ])
 
